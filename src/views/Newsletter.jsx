a061c16b4a759136ff6a20bb6678b6ecc5f4d231
folchzapata <folch@haptic.finance>
2022-04-18 11:47:33 -0400

    Newsletter config

--- src/views/Newsletter.jsx
+++ src/views/Newsletter.jsx
@@ -11,89 +11,119 @@ import {
     useBreakpointValue,
     useColorModeValue,
   } from '@chakra-ui/react'
-  import * as React from 'react'
-  
-  export const Newsletter = () => (
-    <Box bgGradient="linear(to-b, bg-accent 50%, bg-canvas 50%, )">
-      <Container
-        py={{
-          base: '16',
-          md: '24',
-        }}
-      >
-        <Box
-          bg="linear-gradient(-45deg, #4e339a,#33429a)"
-          color="white"
+  import React, {useState} from 'react'
+
+  export const Newsletter = () => {
+    const [emailAddress, setEmailAddress] = useState("");
+    
+    function validateEmail(email)  {
+      return String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    }; 
+
+
+    async function subscribe(event) {
+      if (validateEmail(emailAddress)) {
+        setEmailAddress(emailAddress);
+        console.log(`email is ${emailAddress}`);
+
+      } else {
+        console.log("invalid input")
+      }
+    }
+
+    async function validateEmail(event) {
+      let value = event.target.value;
+
+    }
+
+    return (
+        <Box bgGradient="linear(to-b, bg-accent 50%, bg-canvas 50%, )">
+        <Container
           py={{
-            base: '10',
-            md: '16',
+            base: '16',
+            md: '24',
           }}
-          px={{
-            base: '6',
-            md: '16',
-          }}
-          borderRadius="lg"
-          boxShadow={useColorModeValue('lg', 'lg-dark')}
         >
-          <Stack
-            spacing={{
-              base: '8',
-              md: '8',
+          <Box
+            bg="linear-gradient(-45deg, #4e339a,#33429a)"
+            color="white"
+            py={{
+              base: '10',
+              md: '16',
             }}
-            direction={{
-              base: 'column',
-              lg: 'row',
+            px={{
+              base: '6',
+              md: '16',
             }}
-            justify="space-between"
+            borderRadius="lg"
+            boxShadow={useColorModeValue('lg', 'lg-dark')}
           >
             <Stack
               spacing={{
-                base: '4',
-                md: '5',
+                base: '8',
+                md: '8',
               }}
-            >
-              <Heading
-                size={useBreakpointValue({
-                  base: 'sm',
-                  md: 'md',
-                })}
-              >
-                Be the first to know
-              </Heading>
-              <Text
-                fontSize={{
-                  base: 'lg',
-                  md: 'xl',
-                }}
-                color="subtle"
-              >
-                Stay tuned for protocol announcements
-              </Text>
-            </Stack>
-            <Stack
               direction={{
                 base: 'column',
-                md: 'row',
+                lg: 'row',
               }}
-              spacing="4"
-              width="full"
-              maxW={{
-                base: 'md',
-                xl: 'lg',
-              }}
+              justify="space-between"
             >
-              <FormControl>
-                <Input type="email" size="lg" placeholder="Enter your email" />
-                <FormHelperText color="subtle">We send you at most one mail per month</FormHelperText>
-              </FormControl>
-              <Button variant="primary" size="lg">
-                Subscribe
-              </Button>
+              <Stack
+                spacing={{
+                  base: '4',
+                  md: '5',
+                }}
+              >
+                <Heading
+                  size={useBreakpointValue({
+                    base: 'sm',
+                    md: 'md',
+                  })}
+                >
+                  Be the first to know
+                </Heading>
+                <Text
+                  fontSize={{
+                    base: 'lg',
+                    md: 'xl',
+                  }}
+                  color="subtle"
+                >
+                  Stay tuned for protocol announcements
+                </Text>
+              </Stack>
+              <Stack
+                direction={{
+                  base: 'column',
+                  md: 'row',
+                }}
+                spacing="4"
+                width="full"
+                maxW={{
+                  base: 'md',
+                  xl: 'lg',
+                }}
+              >
+                <FormControl>
+                  <Input type="email" size="lg" placeholder="Enter your email" onChange={validateEmail}/>
+                  <FormHelperText color="subtle">We send you at most one mail per month</FormHelperText>
+                </FormControl>
+                <Button variant="primary" size="lg" onClick={subscribe}>
+                  Subscribe
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
-        </Box>
-      </Container>
-    </Box>
-  )
+          </Box>
+        </Container>
+      </Box>
+    )
+  }
+
+console.log()
 
   export default Newsletter;
