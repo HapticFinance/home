0f2181b57a01a58a612c24949b79de3bf77800b2
Folch <folch@haptic.finance>
2022-04-24 17:01:47 -0400

    Terms of service page

---
+++ src/views/Terms.jsx
@@ -0,0 +1,21 @@
+
+// 1.1 Import Views
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import TermsInfo from "./TermsInfo";
+
+const Terms = () => {
+
+    return(
+        <>
+            <NavBar />
+            <TermsInfo />
+            <Footer />
+        </>
+    )
+}
+
+
+export default Terms;
+
+
