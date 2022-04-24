f3e1eda4448582ca2f8ea99133f203e643c41c47
Folch <folch@haptic.finance>
2022-04-24 17:01:21 -0400

    Privacy Policy page

---
+++ src/views/Privacy.jsx
@@ -0,0 +1,19 @@
+
+// 1.1 Import Views
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import PrivacyInfo from "./PrivacyInfo";
+
+const Privacy = () => {
+
+    return(
+        <>
+            <NavBar />
+            <PrivacyInfo />
+            <Footer />
+        </>
+    )
+}
+
+
+export default Privacy;
